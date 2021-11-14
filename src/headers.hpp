e2b49f837265139948d1974a2db4188602ad7cc7
Juan Burgos <juanlb1988@gmail.com>
2021-11-13 21:09:34 -0500

    Forgot to add top-level header file

---
+++ src/headers.hpp
@@ -0,0 +1,24 @@
+#ifndef _HEADERS_HPP
+#define _HEADERS_HPP
+
+/**
+ * @file headers.hpp
+ * @author Juan Burgos
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-11
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+// Workaround for not using SDL_main() as entrypoint
+#define SDL_MAIN_HANDLED
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include "logger.hpp"
+#include "window_manager.hpp"
+
+#endif /* _HEADERS_HPP */
