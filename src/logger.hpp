0b3275d0aca0509df7c46592549d9dfe06e3c8ce
Juan Burgos <juanlb1988@gmail.com>
2021-11-13 22:52:08 -0500

    Reorganized logger class elements

--- src/logger.hpp
+++ src/logger.hpp
@@ -22,41 +22,36 @@
 
 class Logger
 {
-public:
-    const std::string INFO = "INFO";
-    const std::string WARN = "WARN";
-    const std::string ERROR = "ERROR";
-    const unsigned int FILE_SIZE_LIMIT = 1E4; // 10,000 bytes
-
 private:
     std::string filename;
     unsigned int rotation;
-
-    // Constructors and assignment operator are private for a reason
-    Logger();
-    Logger(const std::string &_filename);
-    Logger(const Logger &);
-    Logger &operator=(const Logger &);
-
     static Logger *logger;
 
 public:
     static Logger *get_logger();
-
-    void set_filename(const std::string &);
-    std::string get_filename_raw() const;
-    std::string get_filename_rotated() const;
-    unsigned int get_file_size();
-
-    void rotate_log();
-    unsigned int get_rotation() const;
-    void set_rotation(unsigned int);
-
     void info(const std::string);
     void warn(const std::string);
     void error(const std::string);
     
 private:
+    const std::string INFO = "INFO";
+    const std::string WARN = "WARN";
+    const std::string ERROR = "ERROR";
+    const unsigned int FILE_SIZE_LIMIT = 1E4; // 10,000 bytes
+
+    // Constructors and assignment operator are private because this is to be a singleton obj
+    Logger();
+    Logger(const std::string &_filename);
+    Logger(const Logger &);
+    Logger &operator=(const Logger &);
+
+    void set_filename(const std::string &);
+    std::string get_filename_raw() const;
+    std::string get_filename_rotated() const;
+    unsigned int get_file_size();
+    void rotate_log();
+    unsigned int get_rotation() const;
+    void set_rotation(unsigned int);
     void write_message_buffer(const std::string, const std::string flag);
 }; 
 
