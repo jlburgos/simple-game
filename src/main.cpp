aae88fa92c446aeb716259e9ddbf3dd05e09e8f6
Juan Burgos <juanlb1988@gmail.com>
2021-11-24 15:50:30 -0500

    Add exception handling for the case where path computation fails

--- src/main.cpp
+++ src/main.cpp
@@ -18,20 +18,28 @@
  */
 int main(void)
 {
-  LOG.info("Starting program...");
-  SDL_SetMainReady();
-  WindowManager mgr;
+    try
+    {
+        LOG.info("Starting program...");
+        SDL_SetMainReady();
+        WindowManager mgr;
 
-  LOG.info("Initializing window manager...");
-  int rc = mgr.init();
-  if (rc != 0)
-  {
-    LOG.error("Failed to initialize program!\n");
-    return rc;
-  }
-  LOG.info("Initialization completed! Bringing up window now...");
-  mgr.start();
+        LOG.info("Initializing window manager...");
+        int rc = mgr.init();
+        if (rc != 0)
+        {
+            LOG.error("Failed to initialize program!\n");
+            return rc;
+        }
+        LOG.info("Initialization completed! Bringing up window now...");
+        mgr.start();
 
-  LOG.info("Program completed");
-  return 0;
+        LOG.info("Program completed");
+        return 0;
+    }
+    catch (const PathNS::PathException &ex)
+    {
+        std::cout << "Exception: " << ex.what() << std::endl;
+        return 1;
+    }
 }
