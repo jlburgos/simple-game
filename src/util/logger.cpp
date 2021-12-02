21b7981d2cf4513a096127e2522632a95aa6511a
Juan Burgos <juanlb1988@gmail.com>
2021-12-01 20:36:04 -0500

    Set up separate thread that handles logging I/O while all other threads write to the shared message queue

--- src/util/logger.cpp
+++ src/util/logger.cpp
@@ -15,6 +15,7 @@
 #include <fstream>
 #include <cstdarg>
 #include <thread>
+#include <chrono>
 
 #include "path.hpp"
 #include "logger.hpp"
@@ -23,6 +24,40 @@
 // Initialize static fields
 Logger *Logger::logger = nullptr;                 // Initialize singleton pointer
 
+void manage_message_queue()
+{
+    std::queue<Logger::Message> *frozen_q = nullptr;
+    std::cout << "Beginning message queue log worker..." << std::endl;
+    while(LOG.is_stay_alive())
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+        // Create a scope for the lock to protect queue assignment
+        {
+            const std::lock_guard<std::mutex> lock(LOG.logger_mutex);
+            if(LOG.active_q == &LOG.q1)
+            {
+                LOG.active_q = &LOG.q2;
+                frozen_q = &LOG.q1;
+            }
+            else
+            {
+                LOG.active_q = &LOG.q1;
+                frozen_q = &LOG.q2;
+            }
+        }
+
+        while(!frozen_q->empty())
+        {
+            std::string msg = LOG.fmt_message(frozen_q->front());
+            std::cout << msg << std::endl;
+            LOG.write_message_buffer(msg);
+            frozen_q->pop();
+        }
+    }
+    std::cout << "Exiting message queue log worker..." << std::endl;
+}
+
 Logger::Logger()
 {
     this->logger_healthy = false;
@@ -30,11 +65,39 @@ Logger::Logger()
     this->rotation = 0;
     this->initialize_log();
     this->logger_healthy = true;
+#if !defined(__EMSCRIPTEN_major__) // Do not write to file if we build as web app
+    this->stay_alive = true;
+    this->initialize_worker_thread();
+#else
+    this->thread = nullptr;
+    this->stay_alive = false;
+#endif
+}
+
+Logger::~Logger()
+{
+    this->close_logger_thread();
+}
+
+void Logger::close_logger_thread()
+{
+    std::cout << "Closing logger worker thread" << std::endl;
+    this->stay_alive = false;
+    if(this->thread != nullptr)
+    {
+        this->thread->join();
+    }
+    std::cout << "Logger thread decomissioned" << std::endl;
 }
 
 bool Logger::is_logger_healthy()
 {
     return this->logger_healthy;
+}
+
+bool Logger::is_stay_alive()
+{
+    return this->stay_alive;
 }
 
 Logger *Logger::get_logger()
@@ -44,6 +107,11 @@ Logger *Logger::get_logger()
         logger = new Logger();
     }
     return logger;
+}
+
+void Logger::initialize_worker_thread()
+{
+    this->thread = std::unique_ptr<std::thread>(new std::thread(manage_message_queue));
 }
 
 std::string Logger::get_filename_raw()
@@ -76,16 +144,16 @@ unsigned int Logger::get_rotation()
     return this->rotation;
 }
 
-void Logger::set_rotation(unsigned int _rotation)
+void Logger::set_rotation(unsigned int rot)
 {
-    this->rotation = _rotation;
+    this->rotation = rot;
 }
 
-unsigned int Logger::get_file_size()
+std::size_t Logger::get_file_size()
 {
     std::ifstream is;
     is.open(this->get_filename_rotated(), std::ifstream::binary);
-    unsigned int file_size = 0;
+    std::size_t file_size = 0;
     if(is)
     {
         is.seekg(0, is.end);
@@ -106,58 +174,82 @@ std::string Logger::get_file_path()
     return PathNS::get_bin_logs_path() + "/" + PathNS::get_exe_name_no_path();
 }
 
-void Logger::info(const std::string message, ...)
+void Logger::info(std::string message, ...)
 {
-    char buffer[256];
+    std::string time = TimerNS::current_time();
+    char buffer[this->BUFFER_LIMIT];
     va_list args;
     va_start(args, message);
     vsprintf(buffer, message.c_str(), args);
     va_end(args);
-    this->write_message_buffer(std::string(buffer), this->PINFO);
+    Message msg = {time, this->PINFO, std::string(buffer)};
+    if(this->stay_alive)
+    {
+        const std::lock_guard<std::mutex> lock(LOG.logger_mutex);
+        active_q->push(msg);
+    }
+    else
+    {
+        std::cout << this->fmt_message(msg) << std::endl;
+    }
 }
 
 void Logger::warn(const std::string message, ...)
 {
-    char buffer[256];
+    std::string time = TimerNS::current_time();
+    char buffer[this->BUFFER_LIMIT];
     va_list args;
     va_start(args, message);
     vsprintf(buffer, message.c_str(), args);
     va_end(args);
-    this->write_message_buffer(std::string(buffer), this->PWARN);
+    Message msg = {time, this->PWARN, std::string(buffer)};
+    if(this->stay_alive)
+    {
+        const std::lock_guard<std::mutex> lock(LOG.logger_mutex);
+        active_q->push(msg);
+    }
+    else
+    {
+        std::cout << this->fmt_message(msg) << std::endl;
+    }
 }
 
 void Logger::error(const std::string message, ...)
 {
-    char buffer[256];
+    std::string time = TimerNS::current_time();
+    char buffer[this->BUFFER_LIMIT];
     va_list args;
     va_start(args, message);
     vsprintf(buffer, message.c_str(), args);
     va_end(args);
-    this->write_message_buffer(std::string(buffer), this->PERROR);
+    Message msg = {time, this->PERROR, std::string(buffer)};
+    if(this->stay_alive)
+    {
+        const std::lock_guard<std::mutex> lock(LOG.logger_mutex);
+        this->active_q->push(msg);
+    }
+    else
+    {
+        std::cout << this->fmt_message(msg) << std::endl;
+    }
 }
 
-void Logger::write_message_buffer(const std::string message, const std::string flag)
+void Logger::write_message_buffer(const std::string message)
 {
-    std::cout << this->message_prefix(flag) << message << std::endl;
-
-// Do not write to file if we build as web app
-#if !defined(__EMSCRIPTEN_major__)
-    const std::lock_guard<std::mutex> lock(this->logger_mutex);
     while(this->get_file_size() > this->FILE_SIZE_LIMIT)
     {
         this->rotate_log();
     }
     std::ofstream ofs;
     ofs.open(this->get_filename_rotated(), std::ofstream::out | std::ofstream::app);
-    ofs << this->message_prefix(flag) << message << std::endl;
+    ofs << message << std::endl;
     ofs.flush();
     ofs.close();
-#endif
 }
 
-std::string Logger::message_prefix(const std::string flag)
+std::string Logger::fmt_message(const Logger::Message &msg)
 {
     std::stringstream ss;
-    ss << "[" << TimerNS::current_time() << "][THREAD " << std::this_thread::get_id() << "] " << flag << ": ";
+    ss << "> [" << msg.timestamp << "][THREAD " << std::this_thread::get_id() << "] " << msg.flag << ": " << msg.msg;
     return ss.str();
 }
