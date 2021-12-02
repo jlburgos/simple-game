56fa714e3c8376b90e5a1cfca0eb249ac77b44e6
Juan Burgos <juanlb1988@gmail.com>
2021-12-02 10:29:10 -0500

    Remove comment

--- src/util/logger.cpp
+++ src/util/logger.cpp
@@ -33,7 +33,6 @@ void manage_message_queue()
     {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-        // Create a scope for the lock to protect queue assignment
         if(LOG.is_stay_alive() || !LOG.q1.empty() || !LOG.q2.empty())
         {
             if(LOG.active_q == &LOG.q1)
