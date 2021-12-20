2d121ca04ece52f2ab2efb3725f3d0059031ba2e
Juan Burgos <juanlb1988@gmail.com>
2021-12-20 13:38:29 -0500

    Add push and pop function definitions

--- src/util/message_queue.cpp
+++ src/util/message_queue.cpp
@@ -17,3 +17,19 @@ std::size_t size()
 {
     return queue.size();
 }
+
+template<class T>
+void push(const T value)
+{
+    const std::lock_guard<std::mutex> lock(LOG.logger_mutex);
+    queue.push(value);
+}
+
+template<class T>
+T pop()
+{
+    const std::lock_guard<std::mutex> lock(LOG.logger_mutex);
+    T value = queue.front();
+    queue.pop();
+    return value;
+}
