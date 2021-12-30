76aae3664d6b0680fb0a9b837792f05a2cf1a734
Juan Burgos <juanlb1988@gmail.com>
2021-12-29 20:30:17 -0500

    Cache path strings so we don't have to recompute every time

--- src/util/path.cpp
+++ src/util/path.cpp
@@ -1,12 +1,12 @@
 /**
  * @file path.cpp
  * @author Juan Burgos
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2021-11-24
- * 
+ *
  * @copyright Copyright (c) 2021
- * 
+ *
  */
 
 #ifdef __linux__
@@ -22,77 +22,95 @@
 
 std::string PathNS::get_exe_path()
 {
-    char buffer[MAX_PATH];
+    if (exe_path.empty())
+    {
+        char buffer[MAX_PATH];
 #ifdef __linux__
-    ssize_t num_bytes = readlink("/proc/self/exe", buffer, MAX_PATH);
-    if (num_bytes < 0 || num_bytes >= MAX_PATH)
-    {
-        throw PathException("Failed in readlink call!");
-    }
+        ssize_t num_bytes = readlink("/proc/self/exe", buffer, MAX_PATH);
+        if (num_bytes < 0 || num_bytes >= MAX_PATH)
+        {
+            throw PathException("Failed in readlink call!");
+        }
 #elif defined(__WIN32)
-    /* Not calling GetModuleFileName(..) since it resolves to GetModuleFIleNameW since UNICODE is defined.
-     * that that would include UNICODE filename support which I don't want to deal with. So calling
-     * GetModuleFIleNameA(..) directly.
-     */
-    DWORD num_bytes = GetModuleFileNameA(NULL, buffer, MAX_PATH);
-    if (num_bytes == 0)
-    {
-        throw PathException("Failed in GetModuleFileNameA call!");
-    }
+        /* Not calling GetModuleFileName(..) since it resolves to GetModuleFIleNameW since UNICODE is defined.
+         * that that would include UNICODE filename support which I don't want to deal with. So calling
+         * GetModuleFIleNameA(..) directly.
+         */
+        DWORD num_bytes = GetModuleFileNameA(NULL, buffer, MAX_PATH);
+        if (num_bytes == 0)
+        {
+            throw PathException("Failed in GetModuleFileNameA call!");
+        }
 #else
 #error "Expected either Linux or Windows platform!"
 #endif
-    buffer[num_bytes] = '\0'; // Need this otherwise we're going to have a bad time
-    std::string path(buffer);
-    for(std::size_t i = 0; i < path.length(); ++i)
-    {
-        path[i] == '\\' ? path[i] = '/' : path[i];
+        buffer[num_bytes] = '\0'; // Need this otherwise we're going to have a bad time
+        std::string path(buffer);
+        for (std::size_t i = 0; i < path.length(); ++i)
+        {
+            path[i] == '\\' ? path[i] = '/' : path[i];
+        }
+        exe_path = path;
     }
-    return path;
+
+    return exe_path;
 }
 
 std::string PathNS::get_exe_path_no_ext()
 {
-    std::string path = PathNS::get_exe_path();
-    std::size_t pos = path.rfind(".exe");
-    if(pos != std::string::npos)
+    if (exe_path_no_ext.empty())
     {
-        path.erase(path.rfind(".exe"));
+        std::string path = PathNS::get_exe_path();
+        std::size_t pos = path.rfind(".exe");
+        if (pos != std::string::npos)
+        {
+            path.erase(path.rfind(".exe"));
+        }
+        exe_path_no_ext = path;
     }
-    return path;
+    return exe_path_no_ext;
 }
 
 std::string PathNS::get_exe_name_no_path()
 {
-    std::string path = PathNS::get_exe_path_no_ext();
-    std::size_t pos = path.rfind("/");
-    if(pos != std::string::npos)
+    if (exe_name_no_path.empty())
     {
-        path = path.substr(pos + 1);
+        std::string path = PathNS::get_exe_path_no_ext();
+        std::size_t pos = path.rfind("/");
+        if (pos != std::string::npos)
+        {
+            path = path.substr(pos + 1);
+        }
+        exe_name_no_path = path;
     }
 
-    return path;
+    return exe_name_no_path;
 }
 
 std::string PathNS::get_root_path()
 {
-    std::string path = PathNS::get_exe_path();
-    std::size_t pos = path.rfind("/");
-
-    // Remove executable name from path
-    if(pos != std::string::npos)
+    if (root_path.empty())
     {
-        path.erase(pos);
+        std::string path = PathNS::get_exe_path();
+        std::size_t pos = path.rfind("/");
+
+        // Remove executable name from path
+        if (pos != std::string::npos)
+        {
+            path.erase(pos);
+        }
+
+        // Remove "bin" directory name from path
+        pos = path.rfind("/");
+        if (pos != std::string::npos)
+        {
+            path.erase(pos);
+        }
+
+        root_path = path;
     }
 
-    // Remove "bin" directory name from path
-    pos = path.rfind("/");
-    if(pos != std::string::npos)
-    {
-        path.erase(pos);
-    }
-
-    return path;
+    return root_path;
 }
 
 std::string PathNS::get_bin_path()
