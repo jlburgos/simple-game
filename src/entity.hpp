ca8e4fcb3c4b26c83fb67db54c32bf9c9f3660e3
Juan Burgos <juanlb1988@gmail.com>
2022-03-26 21:12:49 -0400

    Update testing code for toy entities... starting to figure some things out

--- src/entity.hpp
+++ src/entity.hpp
@@ -11,17 +11,19 @@ private:
     shared_renderer_ptr renderer = nullptr;
 
     // TODO
-    SDL_Rect spriteSrcInTexture;
-    SDL_Rect spriteDstOnSurface;
+    SDL_Rect spriteSrcInTexture = {0,0,0,0};
+    SDL_Rect spriteDstOnSurface = {0,0,0,0};
 
 public:
     Entity() = default;
-    Entity(const Entity&) = default;
+    Entity(const Entity& entity);
     ~Entity() = default;
 
     bool set_entity(shared_renderer_ptr _renderer, const std::string& path);
     shared_renderer_ptr get_renderer() const;
     shared_texture_ptr get_texture() const;
+    SDL_Rect get_src_texture() const;
+    SDL_Rect get_dst_texture() const;
 
     bool set_background(shared_renderer_ptr _renderer, const std::string& path);
 
