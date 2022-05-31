ec814327dffff9237e32611cc51964c636607b80
Juan Burgos <jlburgos@us.ibm.com>
2022-05-31 12:27:46 -0400

    Move scripts to parent directory

--- scripts/run-live-server.bat
+++
@@ -1,2 +0,0 @@
-@echo ON
-npx live-server --entry-file="../bin/game.html" --open="../bin/game.html"
--- scripts/run-live-server.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-npx live-server --entry-file="../bin/game.html" --open="../bin/game.html"
